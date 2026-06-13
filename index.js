a9604434b295c2edc4aac341a15a47daa184a5f0
Venkata Naga Kusal <kottekvnkusal@gmail.com>
2026-06-13 12:45:54 +0530

    added essential features
    completed card details modal,
    fixed the spacing issue in price section
    added total price feature

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const itemsList = document.getElementById('items-list')
 const priceList = document.getElementById('price-list')
 const priceSection = document.getElementById('price-section')
 const completeOrder = document.getElementById('complete-order')
+// const payBtn = document.getElementById('pay-btn')
+const cardDetailsForm = document.getElementById('card-details-form')
+
 
 document.addEventListener("click",function(e){
     menuArray.forEach(function(item){
@@ -17,17 +20,14 @@ document.addEventListener("click",function(e){
     if (e.target.id===`complete-order`){
         confirmOrder()
     }
-    if (e.target.id===`pay-btn`){
-        renderMessage()
-    }
 })
-
-let totalPrice = 0
 
 
 const cardDetailsModal = document.getElementById('card-details-modal')
 
-function renderMessage(){
+
+cardDetailsForm.addEventListener('submit',function(event){
+    event.preventDefault()
     const orderMessageContainer = document.getElementById('message-container')
     const orderMessage = document.getElementById('message-container')
     cardDetailsModal.style.display='none'
@@ -39,26 +39,45 @@ function renderMessage(){
     setTimeout(()=>{
         window.location.reload()
     },5000)
-}
+})
+
+// payBtn.addEventListener('submit',function(e){
+//     // const orderMessageContainer = document.getElementById('message-container')
+//     // const orderMessage = document.getElementById('message-container')
+//     // cardDetailsModal.style.display='none'
+//     // priceSection.style.display='none'
+//     // const inputName = document.getElementById('input-name')
+//     // orderMessage.textContent=`Thanks ${inputName.value}! your order is on the way 
+//     //                          Returning to default state in 5 seconds`
+//     // orderMessageContainer.style.display='flex'
+//     // setTimeout(()=>{
+//     //     window.location.reload()
+//     // },5000)
+// })
 
 
-function removePriceList(id){
-    const itemPricesection = document.getElementById(`price-section-${id}`)
-    itemPricesection.style.display='none'
-}
 
 function confirmOrder(){
     // priceSection.style.display='none'
     completeOrder.style.display='none'
-    totalPrice=0
     cardDetailsModal.style.display='block'
 }
 
-const addedItemsIds=[]
+
+let addedItemsIds=[]
+let totalPrice = 0
+
+function removePriceList(id){
+    const itemPricesection = document.getElementById(`price-section-${id}`)
+    itemPricesection.style.display='none'
+    const remainingItemsIds=addedItemsIds.filter(function(element){
+        return element!=id
+    })
+    addedItemsIds=[...remainingItemsIds]
+}
 
 function renderPrices(id){
 
-    // itemPricesection.style.display='block'
     addedItemsIds.push(id)
     let countOfId=0
     addedItemsIds.forEach(function(itemId){
@@ -67,11 +86,12 @@ function renderPrices(id){
         }
     })
 
-
     const addedItems = menuArray.map(function(item){
 
         if (item.id===id && countOfId===1 && addedItemsIds.includes(id)){
+            console.log(item.price)
             totalPrice+=item.price
+            console.log(`at count 1 total price:${totalPrice}`)
             return `
             <li class='item-price-section' id='price-section-${item.id}'>
                     <p class='added-item-name'>${item.name}</p>
@@ -79,7 +99,7 @@ function renderPrices(id){
                     <p id='count-item-${item.id}' class='item-count'>Count:${countOfId}</p>
                     <p id='price-${item.id}' class='added-item-price'>$${item.price}</p>
             </li>`
-        }else if(countOfId>1){
+        }else if(countOfId>1 && item.id===id){
             const countItems = document.getElementById(`count-item-${id}`)
             const priceEl = document.getElementById(`price-${id}`)
             countItems.textContent = `Count:${countOfId}`
@@ -87,18 +107,30 @@ function renderPrices(id){
             menuArray.forEach(function(menuItem){
                 if (menuItem.id===id){
                     priceOfSingleItem=menuItem.price
-                    totalPrice+=priceOfSingleItem
                 }
             })
             const totalPriceOfItem = priceOfSingleItem*countOfId
             priceEl.textContent = `$${totalPriceOfItem}`
+            totalPrice+=priceOfSingleItem
+            console.log(`after count exceeds 1 price:${totalPrice}`)
         }
-        console.log(totalPrice)
     })
 
     priceSection.style.display='block'
     completeOrder.style.display='block'
     priceList.innerHTML+=addedItems.join('')
+
+    const totalPriceEl = document.getElementById('total-price')
+    totalPriceEl.textContent=`$${totalPrice}`
+
+
+    // remove price section
+
+    // const itemPricesection2 = document.getElementById(`price-section-${id}`)
+    // itemPricesection2.style.display='flex'
+    // console.log(addedItemsIds)
+    
+
 }
 
 
